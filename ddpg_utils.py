5b8e3d3f3c2613761ee6dfce9337663d39f759f8
ChenThree <chenthree2@gmail.com>
2022-04-20 04:14:36 +0800

    [init] ddpg on dkitty

---
+++ ddpg_utils.py
@@ -0,0 +1,230 @@
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.autograd import Variable
+
+from memory import SequentialMemory
+from random_process import OrnsteinUhlenbeckProcess
+
+
+class Actor(nn.Module):
+
+    def __init__(self, num_states, num_actions):
+        super().__init__()
+        self.mlp = nn.Sequential(nn.Linear(num_states, 128), nn.BatchNorm1d(128), nn.LeakyReLU(), nn.Linear(128, 256), nn.BatchNorm1d(256), nn.LeakyReLU(),
+                                 nn.Linear(256, 128), nn.BatchNorm1d(128), nn.LeakyReLU(), nn.Linear(128, num_actions), nn.Tanh())
+        self.init_weights()
+
+    def init_weights(self):
+        # init
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        return self.mlp(x)
+
+
+class Critic(nn.Module):
+
+    def __init__(self, num_states, num_actions):
+        super().__init__()
+        self.mlp1 = nn.Sequential(
+            nn.Linear(num_states, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(),
+        )
+        self.mlp2 = nn.Sequential(
+            nn.Linear(128 + num_actions, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 1),
+        )
+        self.init_weights()
+
+    def init_weights(self):
+        # init
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, xs):
+        x, a = xs
+        out = self.mlp1(x)
+        out = self.mlp2(torch.cat([out, a], 1))
+        return out
+
+
+def to_tensor(ndarray, requires_grad=False, dtype=torch.cuda.FloatTensor):
+    return Variable(torch.from_numpy(ndarray), requires_grad=requires_grad).type(dtype)
+
+
+def soft_update(target, source, tau):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+
+class DDPG(object):
+
+    def __init__(self, num_states, num_actions, args):
+
+        if args.seed > 0:
+            self.seed(args.seed)
+
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        # Create Actor and Critic Network
+        self.actor = Actor(self.num_states, self.num_actions)
+        self.actor_target = Actor(self.num_states, self.num_actions)
+        self.actor_optim = optim.Adam(self.actor.parameters(), lr=args.prate)
+
+        self.critic = Critic(self.num_states, self.num_actions)
+        self.critic_target = Critic(self.num_states, self.num_actions)
+        self.critic_optim = optim.Adam(self.critic.parameters(), lr=args.rate)
+
+        hard_update(self.actor_target, self.actor)  # Make sure target is with the same weight
+        hard_update(self.critic_target, self.critic)
+
+        # Create replay buffer
+        self.memory = SequentialMemory(limit=args.memory_size, window_length=args.window_length)
+        self.random_process = OrnsteinUhlenbeckProcess(size=num_actions, theta=args.ou_theta, mu=args.ou_mu, sigma=args.ou_sigma)
+
+        # Hyper-parameters
+        self.batch_size = args.batch_size
+        self.tau = args.tau
+        self.discount = args.discount
+        self.depsilon = 1.0 / args.epsilon_decay
+
+        self.epsilon = 1.0
+        self.s_t = None  # Most recent state
+        self.a_t = None  # Most recent action
+        self.is_training = True
+
+        self.cuda()
+
+    def update_policy(self):
+        # Sample batch
+        state_batch, action_batch, reward_batch, \
+            next_state_batch, terminal_batch = self.memory.sample_and_split(self.batch_size)
+
+        # Prepare for the target q batch
+        next_q_values = self.critic_target([
+            to_tensor(next_state_batch),
+            self.actor_target(to_tensor(next_state_batch)),
+        ])
+
+        target_q_batch = to_tensor(reward_batch) + \
+            self.discount*to_tensor(terminal_batch.astype(np.float))*next_q_values
+
+        # Critic update
+        self.critic.zero_grad()
+
+        q_batch = self.critic([to_tensor(state_batch), to_tensor(action_batch)])
+
+        criterion = nn.MSELoss()
+        value_loss = criterion(q_batch, target_q_batch)
+        value_loss.backward()
+        self.critic_optim.step()
+
+        # Actor update
+        self.actor.zero_grad()
+
+        policy_loss = -self.critic([to_tensor(state_batch), self.actor(to_tensor(state_batch))])
+
+        policy_loss = policy_loss.mean()
+        policy_loss.backward()
+        self.actor_optim.step()
+
+        # Target update
+        soft_update(self.actor_target, self.actor, self.tau)
+        soft_update(self.critic_target, self.critic, self.tau)
+
+    def eval(self):
+        self.actor.eval()
+        self.actor_target.eval()
+        self.critic.eval()
+        self.critic_target.eval()
+
+    def train(self):
+        self.actor.train()
+        self.actor_target.train()
+        self.critic.train()
+        self.critic_target.train()
+
+    def cuda(self):
+        self.actor.cuda()
+        self.actor_target.cuda()
+        self.critic.cuda()
+        self.critic_target.cuda()
+
+    def observe(self, r_t, s_t1, done):
+        if self.is_training:
+            self.memory.append(self.s_t, self.a_t, r_t, done)
+            self.s_t = s_t1
+
+    def random_action(self):
+        action = np.random.uniform(-1., 1., self.num_actions)
+        self.a_t = action
+        return action
+
+    def select_action(self, s_t, decay_epsilon=True):
+        self.eval()
+        action = self.actor(to_tensor(np.array([s_t])))
+        action = action.cpu().data.numpy().squeeze(0)
+        action += self.is_training * max(self.epsilon, 0) * self.random_process.sample()
+        action = np.clip(action, -1., 1.)
+
+        if decay_epsilon:
+            self.epsilon -= self.depsilon
+
+        self.a_t = action
+        self.train()
+        return action
+
+    def reset(self, obs):
+        self.s_t = obs
+        self.random_process.reset_states()
+
+    def load_weights(self, output):
+        if output is None:
+            return
+
+        self.actor.load_state_dict(torch.load('{}/actor.pkl'.format(output)))
+
+        self.critic.load_state_dict(torch.load('{}/critic.pkl'.format(output)))
+
+    def save_model(self, output):
+        torch.save(self.actor.state_dict(), '{}/actor.pkl'.format(output))
+        torch.save(self.critic.state_dict(), '{}/critic.pkl'.format(output))
+
+    def seed(self, s):
+        torch.manual_seed(s)
+        torch.cuda.manual_seed(s)
---
+++ main copy.py
@@ -0,0 +1,125 @@
+import argparse
+import os
+from copy import deepcopy
+
+import gym
+import numpy as np
+import robel
+
+from ddpg_utils import DDPG
+from evaluator import Evaluator
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+env_name = 'DKittyStandFixed-v0'
+episode_count = 10
+episode_length = 1000
+visualize = True
+
+
+def args_parser():
+    parser = argparse.ArgumentParser(description='DDPG DKiity')
+    parser.add_argument('--mode', default='train', type=str, help='support option: train/test')
+    parser.add_argument('--seed', default=2, type=int, help='random seed')
+    parser.add_argument('--rate', default=0.001, type=float, help='learning rate')
+    parser.add_argument('--prate', default=0.0001, type=float, help='policy net learning rate (only for DDPG)')
+    parser.add_argument('--warmup', default=10000, type=int, help='time without training but only filling the replay memory')
+    parser.add_argument('--discount', default=0.99, type=float, help='')
+    parser.add_argument('--batch-size', default=1024, type=int, help='minibatch size')
+    parser.add_argument('--memory-size', default=6000000, type=int, help='memory size')
+    parser.add_argument('--window_length', default=1, type=int, help='')
+    parser.add_argument('--tau', default=0.001, type=float, help='moving average for target network')
+    parser.add_argument('--ou_theta', default=0.15, type=float, help='noise theta')
+    parser.add_argument('--ou_sigma', default=0.2, type=float, help='noise sigma')
+    parser.add_argument('--ou_mu', default=0.0, type=float, help='noise mu')
+    parser.add_argument('--validate_episodes', default=20, type=int, help='how many episode to perform during validate experiment')
+    parser.add_argument('--max_episode_length', default=500, type=int, help='')
+    parser.add_argument('--validate_steps', default=2000, type=int, help='how many steps to perform a validate experiment')
+    parser.add_argument('--train_iter', default=1000000, type=int, help='train iters each timestep')
+    parser.add_argument('--epsilon_decay', default=50000, type=int, help='linear decay of exploration policy')
+    parser.add_argument('--resume', default='default', type=str, help='Resuming model path for testing')
+    parser.add_argument('--output', default='./checkpoint', type=str, help='output path')
+    return parser.parse_args()
+
+
+def train(agent: DDPG, env, evaluate, args, max_episode_length=None, debug=True):
+
+    agent.is_training = True
+    step = episode = episode_steps = 0
+    episode_reward = 0.
+    observation = None
+    while step < args.train_iter:
+        # reset if it is the start of episode
+        if observation is None:
+            observation = deepcopy(env.reset())
+            agent.reset(observation)
+
+        # agent pick action ...
+        if step <= args.warmup:
+            action = agent.random_action()
+        else:
+            action = agent.select_action(observation)
+
+        # env response with next_observation, reward, terminate_info
+        observation2, reward, done, info = env.step(action)
+        observation2 = deepcopy(observation2)
+        if max_episode_length and episode_steps >= max_episode_length - 1:
+            done = True
+
+        # agent observe and update policy
+        agent.observe(reward, observation2, done)
+        if step > args.warmup:
+            agent.update_policy()
+
+        # [optional] evaluate
+        if evaluate is not None and args.validate_steps > 0 and step % args.validate_steps == 0:
+            policy = lambda x: agent.select_action(x, decay_epsilon=False)
+            validate_reward = evaluate(env, policy, debug=False, visualize=False)
+            if debug:
+                print('[Evaluate] Step_{:07d}: mean_reward:{}'.format(step, validate_reward))
+
+        # [optional] save intermideate model
+        if step % int(args.train_iter / 3) == 0:
+            agent.save_model(args.output)
+
+        # update
+        step += 1
+        episode_steps += 1
+        episode_reward += reward
+        observation = deepcopy(observation2)
+
+        if done:  # end of episode
+            if debug:
+                print('#{}: episode_reward:{} steps:{}'.format(episode, episode_reward, step))
+
+            agent.memory.append(observation, agent.select_action(observation), 0., False)
+
+            # reset
+            observation = None
+            episode_steps = 0
+            episode_reward = 0.
+            episode += 1
+
+
+def main():
+    # read args
+    args = args_parser()
+    # prepare sim env
+    # action: 12 * [-1, 1] observation: 61 * [-inf, inf]
+    env = gym.make(env_name)
+    env.reset()
+    # set random seed
+    np.random.seed(args.seed)
+    env.seed(args.seed)
+
+    nb_states = env.observation_space.shape[0]
+    nb_actions = env.action_space.shape[0]
+
+    agent = DDPG(nb_states, nb_actions, args)
+    evaluate = Evaluator(args.validate_episodes, args.validate_steps, args.output, max_episode_length=args.max_episode_length)
+
+    train(agent, env, evaluate, args, max_episode_length=args.max_episode_length)
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
