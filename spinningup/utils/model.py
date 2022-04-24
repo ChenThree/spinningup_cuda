600565397fb53e217552d3f65913d665ec6964f4
ChenThree <chenthree2@gmail.com>
2022-04-24 21:29:30 +0800

    [update] base model

---
+++ spinningup/utils/model.py
@@ -0,0 +1,23 @@
+import math
+
+import torch.nn as nn
+
+
+class BaseModule(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def init_weights(self):
+        # init
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, (nn.LayerNorm, nn.BatchNorm1d, nn.BatchNorm2d)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
