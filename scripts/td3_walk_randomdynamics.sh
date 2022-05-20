b137579a65017da3ce6b305406dd24eba4e28f48
ChenThree <chenthree2@gmail.com>
2022-05-20 19:12:47 +0800

    [add]

---
+++ scripts/td3_walk_randomdynamics.sh
@@ -0,0 +1,14 @@
+python ../main_td3.py \
+--env DKittyWalkRandomDynamics-v0 \
+--log-dir ../logs-td3-walk-random-lr-0.001-bs-128-delay-2 \
+--batch-size 128 \
+--plr 0.001 \
+--qlr 0.001 \
+--policy-delay 2 \
+--polyak 0.995 \
+--epochs 100 \
+--steps-per-epoch 10000 \
+--warmup 10000 \
+--eps-decay 200000 \
+--update-every 50 \
+--validate-episodes 100
